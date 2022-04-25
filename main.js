c9f46f405fd279c7662025d95173101d8e2bfc86
Lidia Karpinski <lidiakarpinski@gmail.com>
2022-04-25 19:07:54 -0400

    Add refactored selectBoard tile, checkForWin function, update message banner, and draw function

--- main.js
+++ main.js
@@ -1,55 +1,76 @@
 // Variables
-
 var game = new Game();
-var coconut = game.player1.token;
-var pineapple = game.player2.token;
-var currentPlayer = game.currentPlayer;
 
 // Query Selectors
 var boardTile = document.querySelector('#board');
 var messagePrompter = document.querySelector('#messageBox');
 var player1Scoreboard = document.querySelector('#player1Scoreboard');
 var player2Scoreboard = document.querySelector('#player2Scoreboard');
+
 // Event Listeners
 boardTile.addEventListener('click', selectBoardTile)
 
-// Functions and Event Handlers
+// function selectBoardTile(event) {
+//   var target = event.target;
+//   var tileValue = event.target.id
+//   var isTile = target.classList.contains('tile');
+//   var isDisabled = target.classList.contains('disabled')
+//
+//     if (isTile && !isDisabled && !game.currentPlayer.playedTiles.includes(tileValue)) {
+//         if (game.currentPlayer === game.player1) {
+//             target.innerText = game.player1.token;
+//             game.currentPlayer.playedTiles.push(tileValue)
+//           } else {
+//             target.innerText = game.player2.token;
+//             game.currentPlayer.playedTiles.push(tileValue)
+//           }
+//             target.classList.add('disabled')
+//             game.turnCounter++;
+//               game.checkForWin();
+//               game.determineDraw()
+//               game.changePlayerTurn();
+//             }
+//         };
 
 
-
-function resetBoard() {
-      if (game.playedTiles === 9 || this.winner === true) {
-      game.playedTiles = [];
-      target.innerText = '';
-      target.classList.remove('disabled');
-    }
-  }
-function changePlayerTurn() {
-  if (currentPlayer === game.player1) {
-    currentPlayer = game.player2;
-  } else {
-    currentPlayer = game.player1;
-  }
-    messagePrompter.innerText =  `Player ${currentPlayer.token}'s turn!`
-}
 
 function selectBoardTile(event) {
   var target = event.target;
-  var tileValue = event.target.id
+  var tileValue = event.target.id;
   var isTile = target.classList.contains('tile');
-  var isDisabled = target.classList.contains('disabled')
+  var isDisabled = target.classList.contains('disabled');
 
-    if (isTile && !isDisabled && !game.playedTiles.includes(tileValue)) {
-        if (currentPlayer.turn === game.player1.turn) {
-            target.innerText = coconut;
-            game.playedTiles.push(tileValue)
-          } else if (currentPlayer.turn === game.player2.turn) {
-            target.innerText = pineapple;
-            game.playedTiles.push(tileValue)
-          }
-            target.classList.add('disabled')
-            changePlayerTurn();
-        };
-
-        resetBoard();
+  if (isTile && !isDisabled && !game.currentPlayer.playedTiles.includes(tileValue)) {
+    target.innerText = game.currentPlayer.token;
+    game.currentPlayer.playedTiles.push(tileValue);
+    target.classList.add('disabled');
+    game.turnCounter++;
+    game.checkForWin();
+    game.determineDraw();
+    game.changePlayerTurn();
+    updatePrompter();
+  }
 };
+        // if (game.draw === false && game.winner === null) {
+
+function updatePrompter() {
+  if (game.draw === true) {
+    messagePrompter.innerText = `It's a draw! Try again!`
+  } else if (game.win === true) {
+    messagePrompter.innerText = `Player ${game.winner.token} wins! `
+  } else {
+    messagePrompter.innerText = `Player ${game.currentPlayer.token}'s turn!`;
+  }
+}
+        // resetBoard();
+
+        //updateScore();
+
+//         function resetBoard() {
+//               if (game.turnCounter === 9 || game.winner === true) {
+//               game.playedTiles = [];
+//               target.innerText = '';
+//               target.classList.remove('disabled');
+//             }
+//           }
+// };
